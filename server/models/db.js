3ec0c550e30f019c14ef9f0ce3a050e03865979e
Oguz Tugan <t-oguz@tugx.de>
2023-12-08 17:17:51 +0100

    change db

--- server/models/db.js
+++ server/models/db.js
@@ -1,8 +1,21 @@
-const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+dotenv.config();
+const mongoose = require('mongoose');
+
+const MONGOUSER = process.env.MONGOUSER;
+const MONGOPASSWORD = process.env.MONGOPASSWORD;
+const MONGO_URL = process.env.MONGO_URL;
+const MONGOPORT = process.env.MONGOPORT;
+
+const uri = `mongodb://${MONGOUSER}:${MONGOPASSWORD}@${MONGO_URL}:${MONGOPORT}`;
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/diary");
-  console.log("Connected to db ✅");
+  try {
+    await mongoose.connect(uri, { dbName: 'diary' });
+    console.log('MongoDB connected successfully. 🥳');
+  } catch (error) {
+    console.error('Error connecting to MongoDB: 🛑', error);
+  }
 }
 
 module.exports = main;
